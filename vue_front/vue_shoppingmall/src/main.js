d6637395d9e74b53d148074d93ee4df6db31a382
yixinliu <726932055@qq.com>
2020-06-24 22:40:00 +0800

    'fixBug'

--- vue_front/vue_shoppingmall/src/main.js
+++ vue_front/vue_shoppingmall/src/main.js
@@ -3,6 +3,22 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+// import Vue from 'vue'
+// import ECharts from 'vue-echarts' // 在 webpack 环境下指向 components/ECharts.vue
+
+// 手动引入 ECharts 各模块来减小打包体积
+// import 'echarts/lib/chart/bar'
+// import 'echarts/lib/component/tooltip'
+
+// 如果需要配合 ECharts 扩展使用，只需要直接引入扩展包即可
+// 以 ECharts-GL 为例：
+// 需要安装依赖：npm install --save echarts-gl，并添加如下引用
+// import 'echarts-gl'
+
+// 注册组件后即可使用
+// Vue.component('v-chart', ECharts)
+
+
 // 导入组件
 import TreeTable from 'vue-table-with-tree-grid'
 // 导入全局样式表
@@ -19,6 +35,6 @@ Vue.config.productionTip = false
 Vue.component('tree-table', TreeTable)
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
