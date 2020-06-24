8ea84831444a21b9b47fc6ab20c7e8538477006b
rosiekk <1752912956@qq.com>
2020-06-24 22:52:07 +0800

    -

--- foreground/backend/app.py
+++ foreground/backend/app.py
@@ -18,8 +18,7 @@ CORS(app, supports_credentials=True)
 # 初始化数据库连接:
 # engine = create_engine(
 #     'mysql+pymysql://root:zxcxzcz123@localhost:3306/our_mall?charset=utf8')
-engine = create_engine(
-    'mysql+pymysql://root:123456@localhost:3306/test?charset=utf8')
+engine = create_engine(mysql_path)
 # 创建DBSession类型:
 DBSession = sessionmaker(bind=engine)
 # db = SQLAlchemy(app) #实例化
