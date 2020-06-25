dd8fbcb07faf597dc945d44ae94457dc4de9bdc0
yixinliu <726932055@qq.com>
2020-06-25 15:45:36 +0800

    '平台销量接口'

--- backend/app.py
+++ backend/app.py
@@ -913,6 +913,14 @@ def getStatisticsInfo():
     db.close()
     return jsonify(res=[r.to_json() for r in res])
 
+@app.route("/admin/platform_statistics")
+def getPlatformStatistics():
+    res =[{"date":"2020-6-20","sale":999},{"date":"2020-6-21","sale":899},{"date":"2020-6-22","sale":1999},{"date":"2020-6-23","sale":599},{"date":"2020-6-24","sale":799},{"date":"2020-6-25","sale":999},{"date":"2020-6-26","sale":199},{"date":"2020-6-27","sale":99}]
+     # db = DBSession()
+    # res = db.query(UmsMemberStatisticsInfo).all()
+    # db.close()
+    return jsonify(res=res,code=200)
+
 
 if __name__ == '__main__':
     app.debug = True
